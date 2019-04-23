116a84de670c152751d3be9af0eca3582d50b0b7
steel9 <steel9apps@gmail.com>
2019-04-23 18:47:00 +0200

    fix

--- SteelQuiz/QuizCore.cs
+++ SteelQuiz/QuizCore.cs
@@ -299,6 +299,8 @@ namespace SteelQuiz
                         {
                             return false;
                         }
+
+                        File.Delete(PROGRESS_FILE_PATH);
                     }
                     return LoadProgressData();
                 }
