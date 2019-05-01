1a9d974ef246657dc42b6705e44b3b75319dbe3a
steel9 <steel9apps@gmail.com>
2019-05-01 17:54:55 +0200

    updates

--- SteelQuiz/Controls/SmoothFlowLayoutPanel.cs
+++ SteelQuiz/Controls/SmoothFlowLayoutPanel.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+    SteelQuiz - A quiz program designed to make learning words easier
+    Copyright (C) 2019  Steel9Apps
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +27,7 @@ namespace SteelQuiz.Controls
 {
     public class SmoothFlowLayoutPanel : FlowLayoutPanel
     {
+        private Synchronizer synchronizer = new Synchronizer();
         private System.Timers.Timer scrollTimer = null;
         private int scrollElapsed = 0;
         private bool animationRunning = false;
@@ -29,6 +48,8 @@ namespace SteelQuiz.Controls
                 return;
             }
 
+            animationRunning = true;
+
             if (scrollTimer == null)
             {
                 scrollTimer = new System.Timers.Timer
@@ -41,53 +62,55 @@ namespace SteelQuiz.Controls
 
             scrollTimer.Elapsed += delegate
             {
-                System.Diagnostics.Debug.Print("\r\nscrollTimer elapsed START");
-                try
+                lock (synchronizer[scrollElapsed])
                 {
-                    double deltaScrollD = DeltaScroll(scrollElapsed);
-                    if (scrollElapsed > 0 && deltaScrollD == 0)
+                    System.Diagnostics.Debug.Print("\r\nscrollTimer elapsed START");
+                    try
                     {
-                        scrollElapsed = 0;
-                        scrollTimer.Stop();
-                        animationRunning = false;
-                        return;
-                    }
-                    int deltaScroll = Convert.ToInt32(Math.Ceiling(deltaScrollD * e.Delta)) * -1;
-                    if (this.VerticalScroll.Value + deltaScroll < this.VerticalScroll.Minimum)
-                    {
-                        this.VerticalScroll.Value = this.VerticalScroll.Minimum;
+                        double deltaScrollD = DeltaScroll(scrollElapsed);
+                        if (scrollElapsed > 0 && deltaScrollD == 0)
+                        {
+                            scrollElapsed = 0;
+                            scrollTimer.Stop();
+                            animationRunning = false;
+                            return;
+                        }
+                        int deltaScroll = Convert.ToInt32(Math.Ceiling(deltaScrollD * e.Delta)) * -1;
+                        if (this.VerticalScroll.Value + deltaScroll < this.VerticalScroll.Minimum)
+                        {
+                            this.VerticalScroll.Value = this.VerticalScroll.Minimum;
 
-                        scrollElapsed = 0;
-                        scrollTimer.Stop();
-                        animationRunning = false;
-                        return;
-                    }
-                    else if (this.VerticalScroll.Value + deltaScroll > this.VerticalScroll.Maximum)
-                    {
-                        this.VerticalScroll.Value = this.VerticalScroll.Maximum;
+                            scrollElapsed = 0;
+                            scrollTimer.Stop();
+                            animationRunning = false;
+                            return;
+                        }
+                        else if (this.VerticalScroll.Value + deltaScroll > this.VerticalScroll.Maximum)
+                        {
+                            this.VerticalScroll.Value = this.VerticalScroll.Maximum;
 
-                        scrollElapsed = 0;
-                        scrollTimer.Stop();
-                        animationRunning = false;
-                        return;
+                            scrollElapsed = 0;
+                            scrollTimer.Stop();
+                            animationRunning = false;
+                            return;
+                        }
+                        else
+                        {
+                            this.VerticalScroll.Value += deltaScroll;
+                        }
+                        ++scrollElapsed;
                     }
-                    else
+                    finally
                     {
-                        this.VerticalScroll.Value += deltaScroll;
+                        if (scrollElapsed > 0)
+                        {
+                            scrollTimer.Enabled = true;
+                        }
+                        System.Diagnostics.Debug.Print("scrollTimer elapsed STOP");
                     }
-                    ++scrollElapsed;
-                }
-                finally
-                {
-                    if (scrollElapsed > 0)
-                    {
-                        scrollTimer.Enabled = true;
-                    }
-                    System.Diagnostics.Debug.Print("scrollTimer elapsed STOP");
                 }
             };
 
-            animationRunning = true;
             scrollTimer.Start();
         }
 
---
+++ SteelQuiz/Synchronizer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteelQuiz
+{
+    /*
+     * Makes locking value-types possible
+     */ 
+    public class Synchronizer
+    {
+        private static Dictionary<int, object> Locks { get; set; }
+        private object myLock;
+
+        public Synchronizer()
+        {
+            Locks = new Dictionary<int, object>();
+            myLock = new object();
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                lock (myLock)
+                {
+                    object result;
+                    if (Locks.TryGetValue(index, out result))
+                    {
+                        return result;
+                    }
+                    result = new object();
+                    Locks[index] = result;
+                    return result;
+                }
+            }
+        }
+    }
+}
