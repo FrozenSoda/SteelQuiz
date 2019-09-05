10ae357aaa97bfbf9c70e2e7e90f738302fee939
steel9 <steel9apps@gmail.com>
2019-09-05 13:57:08 +0200

    fixed find_files_without_license_header, fixed some other stuff

--- find_files_without_license_header.py
+++ find_files_without_license_header.py
@@ -5,11 +5,10 @@ import os
 def file_list_rec(dir):
 	file_list = list()
 	for (dir_path, dir_names, file_names) in os.walk(dir):
-		file_list += [os.path.join(dir_path, file) for file in file_names]
+		file_list += [os.path.join(dir_path, f) for f in file_names]
 	return file_list
 		
-LICENSE_HEADER = """
-/*
+LICENSE_HEADER = """/*
     SteelQuiz - A quiz program designed to make learning words easier
     Copyright (C) 2019  Steel9Apps
 
@@ -25,20 +24,19 @@ LICENSE_HEADER = """
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-"""
+*/"""
 		
 working_dir = os.path.dirname(os.path.realpath(__file__))
 files = file_list_rec(working_dir)
 files_no_header = list()
 
-for file in files:
+for f in (x for x in files if x.endswith('.cs')):
 	try:
-		if not LICENSE_HEADER in open(file, 'r'):
-			files_no_header.append(file)
+		if not LICENSE_HEADER in open(f, 'r', encoding='utf-8-sig').read():
+			files_no_header.append(f)
 	except UnicodeDecodeError:
 		#found non-text data
-		print("Did not check file ''".format(file))
+		print("Did not check file ''".format(f))
 		
 print('\n\nFiles with missing license header:\n\n')
 print('\n'.join(files_no_header))
