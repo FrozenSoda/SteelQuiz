08ea6bbd4bfcfc740854a70918bde5dce59d5b6e
steel9 <steel9apps@gmail.com>
2020-04-17 21:06:00 +0200

    Copy CurrentCard = QuestionSelector.GenerateCard(Quiz) to KeyPress event

--- SteelQuiz/QuizPractise/InQuiz.cs
+++ SteelQuiz/QuizPractise/InQuiz.cs
@@ -288,6 +288,7 @@ namespace SteelQuiz.QuizPractise
                 var chk = CurrentCard.AnswerCheck(Quiz, CurrentInput, null, !userCopyingAnswer);
                 if (chk.IsCorrect())
                 {
+                    CurrentCard = QuestionSelector.GenerateCard(Quiz); // Generate new card now so that it will be shown on next instance if the user stops practising now
                     newCardPending = true;
                     var correctAnswer = new CorrectAnswer(CurrentCard, Quiz, chk.Certainty);
                     lbl_cardSideToAsk.Controls.Add(correctAnswer);
